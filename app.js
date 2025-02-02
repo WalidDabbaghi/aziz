2e98ca5359ce9cefe40ed9b45db15286449a84fe
waliddb1997 <waliddabbaghi5@gmail.com>
2025-02-02 23:52:33 +0100

    firt

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
